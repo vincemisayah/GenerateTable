21ce53fe6e8d8b469807f8f1dd70e67009d5ac55
Michael Vincent Misayah <mmisayah@fisherprinting.net>
2023-10-31 00:12:45 -0500

    fix update formatting

--- GenerateTable.js
+++ GenerateTable.js
@@ -1,74 +1,55 @@
-// Helper Functions
-const getObjectValue = (object) =>{
-    let val = ""
-    for(const property in object){
-        val += ("<br><br>" + `<b>${property.toUpperCase()}: </b>` + object[property])
-        if(typeof object[property] === 'object'){
-            val += getObjectLowestLevelValue(object[property])
-        }
-    }
-    return val
-}
-
-const getObjectLowestLevelValue = (object) =>{
-    let val = ""
-    for(const property in object){
-        val += ("<br><br>" + `<b>${property.toUpperCase()}: </b>` + object[property])
-        if(typeof object[property] === 'object'){
-            return getObjectLowestLevelValue(object[property])
-        }
-    }
-    return val
-} 
-
-const GenerateTable = (tableId) =>{
-    console.log("Is object")
+/**
+ * 
+ * @param {*} tableId Id of the referenced table
+ * @returns Viewable data in a table format
+ */
+const GenerateTable = (tableId) => {
     // Initialize table components
-    let table = document.getElementById(tableId)
+    let table       = document.getElementById(tableId)
     let tableHeader = table.createTHead();
-    let tableBody = table.createTBody();
+    let tableBody   = table.createTBody();
 
     // Get data from server
-    let fetchedDataPromise = (table.title !== undefined)? getData(table.title):null;
-    if(fetchedDataPromise === null)
+    let fetchedDataPromise = (table.title !== undefined) ? getData(table.title) : null;
+    if (fetchedDataPromise === null)
         return
 
-    fetchedDataPromise.then((res) =>{ // promise status is fulfilled
-        if(res.length === undefined){
+    fetchedDataPromise.then((res) => { // promise status is fulfilled
+        if (res.length === undefined) {
             let row = tableHeader.insertRow(0);
             let index = 0;
-            for(const property in res){
+            for (const property in res) {
                 let headerName = property;
                 let cell = row.insertCell(index++);
                 cell.innerHTML = headerName.toUpperCase()
-    
+
             }
-    
+
             let tableRow = document.createElement("TR");
-    
+
             // Traverse through each value in an object
-            for(const property in res){
+            for (const property in res) {
                 let objValue = res[property]
                 // Create data cell and append it to tableRow
                 let tableData = document.createElement("TD")
-    
+
                 // Recursively acquires the object value down to the lowest level
-                if(typeof objValue === 'object')
+                if (typeof objValue === 'object')
                     tableData.innerHTML = getObjectValue(objValue)
                 else
                     tableData.innerHTML = objValue
                 tableRow.appendChild(tableData)
             }
-    
+
             tableBody.appendChild(tableRow)
         }
-        else{
+        else {
             // Create thead portion of the table
             let row = tableHeader.insertRow(0);
             const firstObject = res[0]
 
             let index = 0;
-            for(const property in firstObject){
+            for (const property in firstObject) {
                 let headerName = property;
                 let cell = row.insertCell(index++);
                 cell.innerHTML = headerName.toUpperCase()
@@ -76,17 +57,17 @@ const GenerateTable = (tableId) =>{
             }
 
             // Populate the table body
-            res.forEach(obj =>{
+            res.forEach(obj => {
                 let tableRow = document.createElement("TR");
 
                 // Traverse through each value in an object
-                for(const property in obj){
+                for (const property in obj) {
                     let objValue = obj[property]
 
                     // Create data cell and append it to tableRow
                     let tableData = document.createElement("TD")
 
-                    if(typeof objValue === 'object')
+                    if (typeof objValue === 'object')
                         tableData.innerHTML = getObjectValue(objValue)
                     else
                         tableData.innerHTML = objValue
@@ -97,13 +78,13 @@ const GenerateTable = (tableId) =>{
                 tableBody.appendChild(tableRow)
             })
         }
-    }).catch((error) =>{ // Data not found
+    }).catch((error) => { // Data not found
         console.log(error)
     })
 }
 
 // Use for callback
-const getData = async (url) =>{
+const getData = async (url) => {
     return await fetchJSON(url);
 }
 
@@ -114,21 +95,50 @@ const getData = async (url) =>{
 async function fetchJSON(requestURL) {
     try {
         const response = await fetch(requestURL);
-
-        if (!response.ok) {
+        if (!response.ok) 
             throw new Error("Network response was not OK");
-        }
-
+        
         const contentType = response.headers.get("content-type");
 
-        if (!contentType || !contentType.includes("application/json")) {
+        if (!contentType || !contentType.includes("application/json")) 
             throw new TypeError("Server did not return a JSON type data.");
-        }
-
+        
         const jsonData = await response.json();
         return jsonData;
     } catch (error) {
         console.error("Error:", error);
     }
 }
+
+/**
+ * @param {*} object data of type 'object'
+ * @returns The value of the object
+ */
+const getObjectValue = (object) => {
+    let val = "<ol>"
+    for (const property in object) {
+        if (typeof object[property] === 'object')
+            val += `<br><strong><u>[${property.toUpperCase()}]</u> ${getObjectLowestLevelValue(object[property])}</strong>`
+        else
+            val += ("<li>" + `<strong><u>${property.toUpperCase()}</u></strong></li> <i>${object[property]}</i>`)
+    }
+    val += "</ol>"
+    return val
+}
+
+/**
+ * @param {*} object data of type 'object' 
+ * @returns The value of inner-most nested object
+ */
+const getObjectLowestLevelValue = (object) => {
+    let val = ""
+    for (const property in object) {
+        val += (`<li>${property.toUpperCase()}: <i style="font-weight:normal">${object[property]}</i></li>`)
+        if (typeof object[property] === 'object')
+            return getObjectLowestLevelValue(object[property])
+    }
+    return val
+}
+
+
 
